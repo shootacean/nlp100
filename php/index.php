17f4dbb353791baa8788850b9a59b3dc283e89bf
Shota Tsukamoto <shootacean@gmail.com>
2020-11-03 09:05:47 +0900

    php q_01

--- php/index.php
+++ php/index.php
@@ -5,4 +5,18 @@ function q_00()
     echo strrev("stressed"), "\n";
 }
 
+function q_01()
+{
+    $word = 'パタトクカシーー';
+    $i = 0;
+    $tmp = [
+        mb_substr($word, $i, 1),
+        mb_substr($word, $i + 2, 1),
+        mb_substr($word, $i + 4, 1),
+        mb_substr($word, $i + 6, 1),
+    ];
+    echo join('', $tmp), "\n";
+}
+
 q_00();
+q_01();
